0bed7df5242268794799de4b137612884a0999c8
NicoleRKiss <64141797+NicoleRKiss@users.noreply.github.com>
2020-07-13 12:02:26 -0300

    Base de Datos ACTUALIZADA

--- globalskills_db_usuarios.sql
+++ globalskills_db_usuarios.sql
@@ -1,20 +1,15 @@
-CREATE TABLE usuarios (
-	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(255) NOT NULL,
-    apellido VARCHAR (255) NOT NULL,
-    email VARCHAR(255) NOT NULL
+CREATE TABLE categorias (
+	id  INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+    nombre VARCHAR(255),
+    descripcion VARCHAR(255),
+    subcategorias VARCHAR(255),
+    cursos VARCHAR (255) -- Aca falta un foreign KEY juntando el precio y descripcion del curso
 );
 
-CREATE TABLE productos (
-	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
-    nombre VARCHAR(255),
-    descripcion VARCHAR(400),
-    precio  SMALLINT
-    
-);
 
-CREATE TABLE carrito (
-	id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE cursos (
+	id  INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
     nombre VARCHAR(255),
-    precio  SMALLINT
+    descripcion VARCHAR(255),
+    precio SMALLINT
 )
